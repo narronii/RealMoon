27e86ba3fe0e0f5a3c0c3a58063bb130ce57fddf
RealMoon <93599571+narronii@users.noreply.github.com>
2021-11-07 18:25:47 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector(".hambmenu");
-
-const navbar = document.querySelector(".navbar")
-
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("change");
-});
-
